8ed94bca9118ce645f20554e0f87008e464eeaa1
John Clampet <jsclampet@gmail.com>
2024-03-22 14:30:48 -0500

    started adding signup functionality

--- server/app.js
+++ server/app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = new express();
-const env = require("dotenv");
 const port = process.env.port || 3004;
 const cors = require("cors");
 const pool = require("./db");
@@ -8,10 +7,25 @@ const pool = require("./db");
 app.use(cors());
 app.use(express.json());
 
-app.get("/messages", async (req, res) => {
+app.get("/chat", async (req, res) => {
   try {
     const allMessages = await pool.query("SELECT * FROM messages");
     res.send(allMessages.rows[0]);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.post("/signup", async (req, res) => {
+  try {
+    const { username } = req.body;
+    const { password } = req.body;
+    const register = await pool.query(
+      "INSERT INTO usernames (username, password) VALUES ($1, $2) RETURNING *",
+      [username, password]
+    );
+
+    res.send(register.rows);
   } catch (error) {
     console.log(error);
   }
